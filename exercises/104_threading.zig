7387911072d0d8f8893e53ab1aca3f827ae6af80
Chris Boesch <chrboesch@noreply.codeberg.org>
2025-11-01 15:46:31 +0100

    fixed more changes due to new I/O API

--- exercises/104_threading.zig
+++ exercises/104_threading.zig
@@ -106,9 +106,7 @@ pub fn main() !void {
 
         // After the threads have been started,
         // they run in parallel and we can still do some work in between.
-        var io_instance: std.Io.Threaded = .init_single_threaded;
-        const io = io_instance.io();
-        try io.sleep(std.Io.Duration.fromSeconds(4), .awake);
+        std.posix.nanosleep(1, 0);
         std.debug.print("Some weird stuff, after starting the threads.\n", .{});
     }
     // After we have left the closed area, we wait until
@@ -119,14 +117,12 @@ pub fn main() !void {
 // This function is started with every thread that we set up.
 // In our example, we pass the number of the thread as a parameter.
 fn thread_function(num: usize) !void {
-    var io_instance: std.Io.Threaded = .init_single_threaded;
-    const io = io_instance.io();
-    try io.sleep(std.Io.Duration.fromSeconds(1 * @as(isize, @intCast(num))), .awake);
+    std.posix.nanosleep(1 * num, 0);
     std.debug.print("thread {d}: {s}\n", .{ num, "started." });
 
     // This timer simulates the work of the thread.
     const work_time = 3 * ((5 - num % 3) - 2);
-    try io.sleep(std.Io.Duration.fromSeconds(@intCast(work_time)), .awake);
+    std.posix.nanosleep(work_time, 0);
 
     std.debug.print("thread {d}: {s}\n", .{ num, "finished." });
 }
